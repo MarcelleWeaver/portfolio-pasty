173ab41ed71eb6f135003b387f25a4bf316a598e
Marcelle <41974324+MichaelWeaver1@users.noreply.github.com>
2026-09-04 21:34:07 -0700

    corrected define config and file path

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,18 +1,21 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginTailwindcss } from '@rsbuild/plugin-tailwindcss';
-import { defineConfig } from '@rsbuild/core';
 
+// Docs: https://rsbuild.rs/config/
 export default defineConfig({
+  plugins: [
+    pluginReact({
+      reactCompiler: true,
+    }),
+    pluginTailwindcss(),
+  
+  ],
   output: {
     // Please replace <REPO_NAME> with the repository name.
     // For example, "/my-project/"
     assetPrefix: '/marcelleweaver.github.io/',
   },
-});
-
-
-
   
   
 });
