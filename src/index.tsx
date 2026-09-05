db00f8419eb17247c6d7b9eee8a3ff5b0261ab79
Marcelle <41974324+MichaelWeaver1@users.noreply.github.com>
2026-09-04 22:11:35 -0700

    bug

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,11 @@ import './css/styles.css';
 import './css/font.css';
 import { HashRouter } from "react-router-dom";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutMe from "./aboutMe";
+import Art from "./art";
+import Design from "./design";
+
 
 const rootEl = document.getElementById('root');
 console.log("finding root")
@@ -14,12 +19,18 @@ if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <section>
-    <p>babababa</p>
-    
-    <React.StrictMode>
-      {/* blank fix 3 */}
-      <App />
-    </React.StrictMode>,
+     <div className="App">
+            <p>wawawawa</p>
+            <HashRouter>
+                <Routes>
+                    <Route  path="/" element={<Art />} />
+                    <Route  path="/art" element={<Art />} />
+
+                    <Route  path="/design" element={<Design />} />
+                    <Route  path="/aboutMe" element={<AboutMe />} />
+                </Routes>
+            </HashRouter>
+        </div>
     </section>
   );
 }
